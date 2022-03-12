e3be763bbe01f404b8f9ec56273a246da83a3844
unknown <mhd84.ir@gmail.com>
2022-03-12 10:48:19 +0330

    tamrin 2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double sum = 0;
+int nomre;
+int m = 0;
+int x = -1;
+System.Console.WriteLine("nomre bede");
+do
+{
+    nomre = Convert.ToInt32(Console.ReadLine());
+    sum = sum + nomre;
+    x++;
+    m = m = nomre;
+} while (nomre <= 20 && nomre != 0);
+System.Console.WriteLine(sum / x);
